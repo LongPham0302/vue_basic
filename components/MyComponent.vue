3d54e36d6b1c9a1b857aa94473d0b1f856b68545
LongPham0302 <bekeo0302@gmail.com>
2024-05-04 08:28:31 +0700

    add

---
+++ components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <slot></slot>
+    <button>
+    <slot name="icon"></slot>
+    <slot name="content"></slot>
+  </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+}
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
---
+++ pages/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul class="skip-links">
+    <li>
+      <a href="#main" ref="skipLink" class="skip-link">Skip to main content</a>
+    </li>
+  </ul>
+  <MyComponent>
+    <h1 id="main">Main content</h1>
+    <template v-slot:icon>
+        <h2>This is the icon</h2>
+      </template>
+      <template v-slot:content>
+        <h2>This is the content</h2>
+      </template>
+  </MyComponent>
+  </div>
+</template>
+
+<script>
+import MyComponent from "../../components/MyComponent.vue";
+
+export default {
+  components: {
+    MyComponent,
+  },  
+  data() {
+    return {
+      message: "Xin chào thế giới!",
+    };
+  },
+};
+</script>
